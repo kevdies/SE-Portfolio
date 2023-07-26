9559add515efc4c1538c1d02770aa08b7a263d74
Kevin Diesenberg <kdiesenb@gmail.com>
2023-07-26 16:26:12 -0400

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#skills">Skills</a>
+      <a href="#resume">Resume</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
