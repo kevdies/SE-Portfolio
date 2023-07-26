e4737460e8e1e7d224704c9f200271743e66c1ce
Kevin Diesenberg <kdiesenb@gmail.com>
2023-07-26 16:46:18 -0400

    basic navigation set up

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,28 @@
+import React from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
 const Navigation = () => {
   return (
-    <nav>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#skills">Skills</a>
-      <a href="#resume">Resume</a>
-      <a href="#contact">Contact</a>
-    </nav>
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">K E V I N D I E S E N B E R G</NavbarBrand>
+      <Nav className="mr-auto" navbar>
+        <NavItem>
+          <NavLink href="#about">About</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#projects">Projects</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#skills">Skills</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#resume">Resume</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#contact">Contact</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 };
 
